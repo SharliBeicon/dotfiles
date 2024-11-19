1308bb0a7c1872231428ed5a30455f47960e8635
Carlos Tocino <ct@databeacon.aero>
2024-11-19 21:27:17 +0100

    alacritty to wezterm switch

---
+++ wezterm/.wezterm.lua
@@ -0,0 +1,13 @@
+local wezterm = require("wezterm")
+
+local config = wezterm.config_builder()
+
+config.color_scheme = "rose-pine"
+config.font = wezterm.font("Iosevka Nerd Font")
+config.font_size = 17.0
+config.window_background_opacity = 0.95
+config.default_prog = { "/bin/zsh", "-l", "-c", "zellij" }
+config.enable_tab_bar = false
+config.window_decorations = "RESIZE | MACOS_FORCE_ENABLE_SHADOW"
+
+return config
